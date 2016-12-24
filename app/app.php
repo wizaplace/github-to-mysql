261635b274bda09481cca0985f6b75e652fce923
Matthieu Napoli <matthieu@mnapoli.fr>
2016-12-24 16:10:14 +0100

    Extract DB schema to a separate file for simplification

--- app/app.php
+++ app/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use Doctrine\DBAL\Schema\Schema;
 use Dotenv\Dotenv;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
@@ -9,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Load .env file
+// Load .env file if it exists
 if (is_file(getcwd() . '/.env')) {
     $dotenv = new Dotenv(getcwd());
     $dotenv->load();
@@ -175,62 +174,10 @@ MYSQL;
 });
 
 $app->command('db-init [--force]', function ($force, OutputInterface $output) use ($db) {
-    $schema = new Schema();
-
-    // Labels
-    $labelsTable = $schema->createTable('github_labels');
-    $labelsTable->addColumn('id', 'integer', ['unsigned' => true]);
-    $labelsTable->addColumn('url', 'string');
-    $labelsTable->addColumn('name', 'string');
-    $labelsTable->addColumn('color', 'string');
-    $labelsTable->setPrimaryKey(['id']);
-    $labelsTable->addIndex(['name']);
-    // Milestones
-    $milestonesTable = $schema->createTable('github_milestones');
-    $milestonesTable->addColumn('id', 'integer', ['unsigned' => true]);
-    $milestonesTable->addColumn('title', 'string');
-    $milestonesTable->addColumn('description', 'string');
-    $milestonesTable->addColumn('url', 'string');
-    $milestonesTable->addColumn('open', 'boolean');
-    $milestonesTable->setPrimaryKey(['id']);
-    $milestonesTable->addIndex(['title']);
-    // Issues
-    $issuesTable = $schema->createTable('github_issues');
-    $issuesTable->addColumn('id', 'integer', ['unsigned' => true]);
-    $issuesTable->addColumn('title', 'text');
-    $issuesTable->addColumn('open', 'boolean');
-    $issuesTable->addColumn('author', 'string');
-    $issuesTable->addColumn('author_avatar_url', 'string', ['notnull' => false]);
-    $issuesTable->addColumn('created_at', 'datetime');
-    $issuesTable->addColumn('updated_at', 'datetime');
-    $issuesTable->addColumn('closed_at', 'datetime', ['notnull' => false]);
-    $issuesTable->addColumn('is_pull_request', 'boolean');
-    $issuesTable->addColumn('milestone_id', 'integer', ['unsigned' => true, 'notnull' => false]);
-    $issuesTable->setPrimaryKey(['id']);
-    $issuesTable->addIndex(['author']);
-    $issuesTable->addIndex(['open']);
-    $issuesTable->addIndex(['created_at']);
-    $issuesTable->addIndex(['updated_at']);
-    $issuesTable->addIndex(['closed_at']);
-    $issuesTable->addIndex(['is_pull_request']);
-    $issuesTable->addForeignKeyConstraint('github_milestones', ['milestone_id'], ['id']);
-    // Labels
-    $issueLabelsTable = $schema->createTable('github_issue_labels');
-    $issueLabelsTable->addColumn('issue_id', 'integer', ['unsigned' => true]);
-    $issueLabelsTable->addColumn('label_id', 'integer', ['unsigned' => true]);
-    $issueLabelsTable->setPrimaryKey(['issue_id', 'label_id']);
-    $issueLabelsTable->addForeignKeyConstraint('github_issues', ['issue_id'], ['id'], [
-        'onUpdate' => 'CASCADE',
-        'onDelete' => 'CASCADE',
-    ]);
-    $issueLabelsTable->addForeignKeyConstraint('github_labels', ['label_id'], ['id'], [
-        'onUpdate' => 'CASCADE',
-        'onDelete' => 'CASCADE',
-    ]);
-
+    $targetSchema = require __DIR__ . '/db-schema.php';
     $currentSchema = $db->getSchemaManager()->createSchema();
 
-    $migrationQueries = $currentSchema->getMigrateToSql($schema, $db->getDatabasePlatform());
+    $migrationQueries = $currentSchema->getMigrateToSql($targetSchema, $db->getDatabasePlatform());
 
     $db->transactional(function () use ($migrationQueries, $force, $output, $db) {
         foreach ($migrationQueries as $query) {
---
+++ app/db-schema.php
@@ -0,0 +1,56 @@
+<?php
+
+$schema = new Doctrine\DBAL\Schema\Schema();
+
+// Labels
+$labelsTable = $schema->createTable('github_labels');
+$labelsTable->addColumn('id', 'integer', ['unsigned' => true]);
+$labelsTable->addColumn('url', 'string');
+$labelsTable->addColumn('name', 'string');
+$labelsTable->addColumn('color', 'string');
+$labelsTable->setPrimaryKey(['id']);
+$labelsTable->addIndex(['name']);
+// Milestones
+$milestonesTable = $schema->createTable('github_milestones');
+$milestonesTable->addColumn('id', 'integer', ['unsigned' => true]);
+$milestonesTable->addColumn('title', 'string');
+$milestonesTable->addColumn('description', 'string');
+$milestonesTable->addColumn('url', 'string');
+$milestonesTable->addColumn('open', 'boolean');
+$milestonesTable->setPrimaryKey(['id']);
+$milestonesTable->addIndex(['title']);
+// Issues
+$issuesTable = $schema->createTable('github_issues');
+$issuesTable->addColumn('id', 'integer', ['unsigned' => true]);
+$issuesTable->addColumn('title', 'text');
+$issuesTable->addColumn('open', 'boolean');
+$issuesTable->addColumn('author', 'string');
+$issuesTable->addColumn('author_avatar_url', 'string', ['notnull' => false]);
+$issuesTable->addColumn('created_at', 'datetime');
+$issuesTable->addColumn('updated_at', 'datetime');
+$issuesTable->addColumn('closed_at', 'datetime', ['notnull' => false]);
+$issuesTable->addColumn('is_pull_request', 'boolean');
+$issuesTable->addColumn('milestone_id', 'integer', ['unsigned' => true, 'notnull' => false]);
+$issuesTable->setPrimaryKey(['id']);
+$issuesTable->addIndex(['author']);
+$issuesTable->addIndex(['open']);
+$issuesTable->addIndex(['created_at']);
+$issuesTable->addIndex(['updated_at']);
+$issuesTable->addIndex(['closed_at']);
+$issuesTable->addIndex(['is_pull_request']);
+$issuesTable->addForeignKeyConstraint('github_milestones', ['milestone_id'], ['id']);
+// Labels
+$issueLabelsTable = $schema->createTable('github_issue_labels');
+$issueLabelsTable->addColumn('issue_id', 'integer', ['unsigned' => true]);
+$issueLabelsTable->addColumn('label_id', 'integer', ['unsigned' => true]);
+$issueLabelsTable->setPrimaryKey(['issue_id', 'label_id']);
+$issueLabelsTable->addForeignKeyConstraint('github_issues', ['issue_id'], ['id'], [
+    'onUpdate' => 'CASCADE',
+    'onDelete' => 'CASCADE',
+]);
+$issueLabelsTable->addForeignKeyConstraint('github_labels', ['label_id'], ['id'], [
+    'onUpdate' => 'CASCADE',
+    'onDelete' => 'CASCADE',
+]);
+
+return $schema;
